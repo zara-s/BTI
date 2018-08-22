ca6bf787360c5393e52f2be0b475ec9a7360961d
Zara Smith <Zara@Zaras-MacBook-Pro.local>
2018-08-22 12:13:11 +1000

    new practise matt thing

--- 22:8 idk.R
+++ 22:8 idk.R
@@ -4,10 +4,10 @@ library(tidyverse)
 
 
 diamonds2 <- diamonds %>% 
-  filter(color == G & depth > 60) %>% 
+  filter(color == "G" & depth > 60) %>% 
   group_by(clarity) %>% 
   arrange(clarity) %>% 
-  mutate(av_carat = mean(carot))
+  mutate(av_carat = mean(carat))
 
-ggplot(diamonds2, aes(car0t, price)) %>% 
-  geom_point(aes(colour=cut size=table, alpha=1/10)) +
+ggplot(diamonds2, aes(carat, price)) +
+  geom_point(aes(colour=cut, size=table, alpha=1/10)) 
